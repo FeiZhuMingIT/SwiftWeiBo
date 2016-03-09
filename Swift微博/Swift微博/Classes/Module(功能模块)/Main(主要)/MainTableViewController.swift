2789f5b1ab8e1f33a3f128f6e03cef56b13b9c83
HMFifth <WZZ@wangzhizhongMBP.lan>
2016-03-09 21:46:12 +0800

    设置基本功能框架

--- Swift微博/Swift微博/Classes/Module(功能模块)/Main(主要)/MainTableViewController.swift
+++ Swift微博/Swift微博/Classes/Module(功能模块)/Main(主要)/MainTableViewController.swift
@@ -8,88 +8,30 @@
 
 import UIKit
 
-class MainTableViewController: UITableViewController {
-
+class MainTableViewController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
+        addChildViewController(HomeTableViewController(), title: "消息", imageName: "tabbar_message_center")
+        addChildViewController(HomeTableViewController(), title: "发现", imageName: "tabbar_discover")
+        addChildViewController(HomeTableViewController(), title: "我", imageName: "tabbar_profile")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    ///  添加控制器
+    private func addChildViewController(vc:UIViewController, title: String, imageName: String) {
+        // 选中状态下的颜色
+        tabBar.tintColor = UIColor.orangeColor()
+        
+        let homeVc = HomeTableViewController()
+        homeVc.tabBarItem.image = UIImage(named: imageName)
+        homeVc.tabBarItem.selectedImage = UIImage(named: imageName+"_highlighted")
+        homeVc.title = title
+        
+        let nav = UINavigationController(rootViewController: homeVc)
+        self.addChildViewController(nav)
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
