f659ec2d1af3ad33edd1a92a1f29e5809ad82cdf
HMFifth <WZZ@wangzhizhongMBP.lan>
2016-03-09 21:18:19 +0800

    创建框架文件

--- Swift微博/Swift微博/AppDelegate.swift
+++ Swift微博/Swift微博/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.backgroundColor = UIColor.whiteColor()
-        window?.rootViewController = ViewController()
+        window?.rootViewController = MainTableViewController()
         window?.makeKeyAndVisible()
         
         return true
